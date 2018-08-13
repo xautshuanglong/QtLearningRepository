9fed64478b6ffda0697936d505fdc6e67148d740
xautshuanglong <xjshuanglong@126.com>
2018-08-13 23:51:55 +0800

    modify qss

--- QtGuiApplicationTest/QtGuiApplicationTest/MyApplication.cpp
+++ QtGuiApplicationTest/QtGuiApplicationTest/MyApplication.cpp
@@ -6,8 +6,8 @@ MyApplication::MyApplication(int &argc, char **argv, int appFlag /* = Applicatio
     : QApplication(argc, argv, appFlag)
 {
     QFile mainTabStyle(":/QtGuiApplicationTest/Resources/qss/white.qss");
-//     QFile mainTabStyle(":/QtGuiApplicationTest/Resources/qss/dark_orange.qss");
-//     QFile mainTabStyle(":/QtGuiApplicationTest/Resources/qss/black.qss");
+    //QFile mainTabStyle(":/QtGuiApplicationTest/Resources/qss/dark_orange.qss");
+    //QFile mainTabStyle(":/QtGuiApplicationTest/Resources/qss/black.qss");
     if (mainTabStyle.open(QFile::ReadOnly))
     {
         QString styleSheet = QLatin1String(mainTabStyle.readAll());
