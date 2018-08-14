b47f5f74289f456fc63853402ccfac445c168019
xautshuanglong <xjshuanglong@126.com>
2018-08-15 00:46:16 +0800

    how to split window

--- QtGuiApplicationTest/QtGuiApplicationTest/MainTabPageDicom.cpp
+++ QtGuiApplicationTest/QtGuiApplicationTest/MainTabPageDicom.cpp
@@ -7,9 +7,20 @@ MainTabPageDicom::MainTabPageDicom(QWidget *parent /* = Q_NULLPTR */)
     : QWidget(parent)
 {
     ui.setupUi(this);
-    this->setStyleSheet("background: green;");
+    //this->setStyleSheet("background: green;");
+
+    ui.dockDcmTagContents->setStyleSheet("background:green;");
+    ui.tableWidgetDcmTag->setStyleSheet("background:red;");
 }
 
 MainTabPageDicom::~MainTabPageDicom()
 {
+}
+
+void MainTabPageDicom::on_testPushButton_clicked()
+{
+    if (!ui.dockDcmTag->isVisible())
+    {
+        ui.dockDcmTag->show();
+    }
 }
--- QtGuiApplicationTest/QtGuiApplicationTest/MainTabPageDicom.h
+++ QtGuiApplicationTest/QtGuiApplicationTest/MainTabPageDicom.h
@@ -6,11 +6,17 @@
 
 class MainTabPageDicom : public QWidget
 {
+    Q_OBJECT
+
 private:
     Ui::MainTabPageDicom ui;
+
 public:
     explicit MainTabPageDicom(QWidget *parent = Q_NULLPTR);
     ~MainTabPageDicom();
+
+ private slots:
+    void on_testPushButton_clicked();
 };
 
 #endif MAIN_TAB_PAGE_DICOM_H
