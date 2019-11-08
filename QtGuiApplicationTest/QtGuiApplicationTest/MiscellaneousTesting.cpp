39fae701177a360d6410b945ae77ab78fb5e6efa
xjshuanglong <xjshuanglong@126.com>
2019-11-08 16:50:02 +0800

    WinApi _beginthreadex + WaitForSingleObject 测试

--- QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousTesting.cpp
+++ QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousTesting.cpp
@@ -1,5 +1,8 @@
 #include "MiscellaneousTesting.h"
 
+#include <windows.h>
+#include <process.h>
+
 #include "ui_MiscellaneousTesting.h"
 #include "FramelessWindowHelper.h"
 
@@ -7,6 +10,8 @@ MiscellaneousTesting::MiscellaneousTesting(QWidget *parent /* = Q_NULLPTR */)
     : QMainWindow(parent)
     , mpFramelessHelper(Q_NULLPTR)
     , ui(new Ui::MiscellaneousTesting())
+    , m_beginThreadHandle(0)
+    , m_threadStopFlag(false)
 {
     ui->setupUi(this);
     mpFramelessHelper = new FramelessWindowHelper(this);
@@ -23,4 +28,39 @@ MiscellaneousTesting::~MiscellaneousTesting()
 void MiscellaneousTesting::closeEvent(QCloseEvent *event)
 {
     emit SignalClosed();
+}
+
+void MiscellaneousTesting::on_btnWinApiThreadStart_clicked()
+{
+    m_threadStopFlag = false;
+    m_beginThreadHandle = _beginthreadex(NULL, 0, MiscellaneousTesting::ThreadProc, (void*)this, 0, NULL);
+}
+
+void MiscellaneousTesting::on_btnWinApiThreadStop_clicked()
+{
+    m_threadStopFlag = true;
+    ::WaitForSingleObject((HANDLE)m_beginThreadHandle, INFINITE);
+    int i = 0;
+}
+
+void MiscellaneousTesting::run()
+{
+    int count = 0;
+    while (count < 1000)
+    {
+        ::_sleep(10);
+        ++count;
+        if (m_threadStopFlag)
+        {
+            break;
+        }
+    }
+    int i = 0;
+}
+
+unsigned int _stdcall MiscellaneousTesting::ThreadProc(void *pArg)
+{
+    MiscellaneousTesting *pMiscellaneous = (MiscellaneousTesting*)pArg;
+    pMiscellaneous->run();
+    return 0;
 }
--- QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousTesting.h
+++ QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousTesting.h
@@ -15,6 +15,9 @@ class MiscellaneousTesting : public QMainWindow
 private:
     Ui::MiscellaneousTesting   *ui;
     FramelessWindowHelper      *mpFramelessHelper;
+    // WinAPI 线程测试
+    unsigned long long          m_beginThreadHandle; // _beginthreadex 返回值，线程具备，用于 WaitForSingleObject
+    bool                        m_threadStopFlag;
 
 public:
     explicit MiscellaneousTesting(QWidget *parent = Q_NULLPTR);
@@ -23,6 +26,14 @@ public:
 protected:
     virtual void closeEvent(QCloseEvent *event) override;
 
+private:
+    static unsigned int _stdcall ThreadProc(void *pArg);
+    void run();
+
 signals:
     void SignalClosed();
+
+private slots:
+    void on_btnWinApiThreadStart_clicked();
+    void on_btnWinApiThreadStop_clicked();
 };
