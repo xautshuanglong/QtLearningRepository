b901a32a85bcc56661d3131135855c8744fc916d
Shuanglong <Shuanglong@XAUTSHUANGLONG>
2019-12-08 22:22:37 +0800

    QT 信号槽可以直接传递 QSharedPointer<QObject> 类型参数

--- QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousSignalSlot.cpp
+++ QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousSignalSlot.cpp
@@ -4,8 +4,21 @@
 MiscellaneousSignalSlot::MiscellaneousSignalSlot(QWidget *parent)
     : MiscellaneousBase(parent)
     , ui(new Ui::MiscellaneousSignalSlot())
+    , pObjTestSignals(new QObject(this))
 {
     ui->setupUi(this);
+
+    pObjTestSignals->setObjectName("test_signal_pointer");
+    pObjTestSignals->setProperty("INT", 110);
+    pObjTestSignals->setProperty("STRING", "Hello World.");
+
+    bool resultFlag = false;
+    resultFlag = this->connect(this, SIGNAL(SignalMainThreadVoid()), this, SLOT(SlotMainThreadVoid()));
+    resultFlag = this->connect(this, SIGNAL(SignalMainThreadInteger(int)), this, SLOT(SlotMainThreadInteger(int)));
+    resultFlag = this->connect(this, SIGNAL(SignalMainThreadIntegerSharedPointer(QSharedPointer<int>)), this, SLOT(SlotMainThreadIntegerSharedPointer(QSharedPointer<int>)));
+    resultFlag = this->connect(this, SIGNAL(SignalMainThreadQbject(const QObject&)), this, SLOT(SlotMainThreadQbject(const QObject&)));
+    resultFlag = this->connect(this, SIGNAL(SignalMainThreadQbjectPointer(QObject*)), this, SLOT(SlotMainThreadQbjectPointer(QObject*)));
+    resultFlag = this->connect(this, SIGNAL(SignalMainThreadQbjectSharedPointer(QSharedPointer<QObject>)), this, SLOT(SlotMainThreadQbjectSharedPointer(QSharedPointer<QObject>)));
 }
 
 MiscellaneousSignalSlot::~MiscellaneousSignalSlot()
@@ -13,9 +26,65 @@ MiscellaneousSignalSlot::~MiscellaneousSignalSlot()
     delete ui;
 }
 
-void MiscellaneousSignalSlot::on_btnEmitSignalMainThread_clicked()
+void MiscellaneousSignalSlot::SlotMainThreadVoid()
 {
     int i = 0;
+}
+
+void MiscellaneousSignalSlot::SlotMainThreadInteger(int testInt)
+{
+    int i = 0;
+}
+
+void MiscellaneousSignalSlot::SlotMainThreadIntegerSharedPointer(QSharedPointer<int> pTestInt)
+{
+    int testInt = *pTestInt;
+    int i = 0;
+}
+
+void MiscellaneousSignalSlot::SlotMainThreadQbject(const QObject& testObj)
+{
+    int testInt = testObj.property("INT").toInt();
+    QString objName = testObj.objectName();
+    QString testString = testObj.property("STRING").toString();
+    int i = 0;
+}
+
+void MiscellaneousSignalSlot::SlotMainThreadQbjectPointer(QObject* pTestObj)
+{
+    int testInt = pTestObj->property("INT").toInt();
+    QString objName = pTestObj->objectName();
+    QString testString = pTestObj->property("STRING").toString();
+    int i = 0;
+}
+
+void MiscellaneousSignalSlot::SlotMainThreadQbjectSharedPointer(QSharedPointer<QObject> pTestObj)
+{
+    int testInt = pTestObj->property("INT").toInt();
+    QString objName = pTestObj->objectName();
+    QString testString = pTestObj->property("STRING").toString();
+    int i = 0;
+}
+
+void MiscellaneousSignalSlot::on_btnEmitSignalMainThread_clicked()
+{
+    emit SignalMainThreadVoid();
+    emit SignalMainThreadInteger(110);
+    emit SignalMainThreadIntegerSharedPointer(QSharedPointer<int>(new int(119)));
+
+    QObject testObject;
+    testObject.setObjectName("test_signal");
+    testObject.setProperty("INT", 110);
+    testObject.setProperty("STRING", "Hello World.");
+    emit SignalMainThreadQbject(testObject);
+
+    emit SignalMainThreadQbjectPointer(pObjTestSignals);
+
+    QSharedPointer<QObject> spTestObj(new QObject(this));
+    spTestObj->setObjectName("test_signal_pointer_shared");
+    spTestObj->setProperty("INT", 110);
+    spTestObj->setProperty("STRING", "Hello world, shared pointer!");
+    emit SignalMainThreadQbjectSharedPointer(spTestObj);
 }
 
 void MiscellaneousSignalSlot::on_btnEmitSignalSubThread_clicked()
--- QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousSignalSlot.h
+++ QtGuiApplicationTest/QtGuiApplicationTest/MiscellaneousSignalSlot.h
@@ -19,11 +19,26 @@ public:
     virtual MiscellaneousTestItem GetItemID() override;
 
 private slots:
+    void SlotMainThreadVoid();
+    void SlotMainThreadInteger(int testInt);
+    void SlotMainThreadIntegerSharedPointer(QSharedPointer<int> pTestInt);
+    void SlotMainThreadQbject(const QObject& testObj);
+    void SlotMainThreadQbjectPointer(QObject* pTestObj);
+    void SlotMainThreadQbjectSharedPointer(QSharedPointer<QObject> pTestObj);
     void on_btnEmitSignalMainThread_clicked();
     void on_btnEmitSignalSubThread_clicked();
 
+signals:
+    void SignalMainThreadVoid();
+    void SignalMainThreadInteger(int testInt);
+    void SignalMainThreadIntegerSharedPointer(QSharedPointer<int> pTestInt);
+    void SignalMainThreadQbject(const QObject& testObj);
+    void SignalMainThreadQbjectPointer(QObject* pTestObj);
+    void SignalMainThreadQbjectSharedPointer(QSharedPointer<QObject> pTestObj);
+
 private:
     Ui::MiscellaneousSignalSlot *ui;
+    QObject                     *pObjTestSignals;
 };
 
 #endif // MISCELLANEOUS_SIGNAL_SLOT_H
