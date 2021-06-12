e261794ad2cbf0a1fa90f5a0953ce54dbca28e60
Shuanglong <xjshuanglong@126.com>
2021-06-12 13:28:36 +0800

    重命名文件，分门别类。

--- QtGuiApplicationTest/QtQuickApplicationTest/main.cpp
+++ QtGuiApplicationTest/QtQuickApplicationTest/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/MainWindow.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
