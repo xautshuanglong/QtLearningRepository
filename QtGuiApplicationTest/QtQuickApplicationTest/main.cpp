d416c8cf56bf1a31c685c66485bafb58a877292c
Shuanglong <xjshuanglong@126.com>
2021-06-15 00:01:14 +0800

    quick 插件拷贝

--- QtGuiApplicationTest/QtQuickApplicationTest/main.cpp
+++ QtGuiApplicationTest/QtQuickApplicationTest/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/MainWindow.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/UI/MainWindow.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
