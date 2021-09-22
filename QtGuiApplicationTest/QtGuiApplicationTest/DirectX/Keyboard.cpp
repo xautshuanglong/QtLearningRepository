8aa6d2e50c4c5f25772ee2241cb2995465da28df
Shuanglong <xjshuanglong@126.com>
2021-09-22 09:39:12 +0800

    代码格式化

--- QtGuiApplicationTest/QtGuiApplicationTest/DirectX/Keyboard.cpp
+++ QtGuiApplicationTest/QtGuiApplicationTest/DirectX/Keyboard.cpp
@@ -14,7 +14,6 @@
 #include "Keyboard.h"
 
 
-
 using namespace DirectX;
 using Microsoft::WRL::ComPtr;
 
@@ -26,29 +25,28 @@ static_assert(sizeof(Keyboard::State) == (256 / 8), "Size mismatch for State");
 
 namespace
 {
-	void KeyDown(int key, Keyboard::State& state)
-	{
-		if (key < 0 || key > 0xfe)
-			return;
+    void KeyDown(int key, Keyboard::State& state)
+    {
+        if (key < 0 || key > 0xfe)
+            return;
 
-		auto ptr = reinterpret_cast<uint32_t*>(&state);
+        auto ptr = reinterpret_cast<uint32_t*>(&state);
 
-		unsigned int bf = 1u << (key & 0x1f);
-		ptr[(key >> 5)] |= bf;
-	}
+        unsigned int bf = 1u << (key & 0x1f);
+        ptr[(key >> 5)] |= bf;
+    }
 
-	void KeyUp(int key, Keyboard::State& state)
-	{
-		if (key < 0 || key > 0xfe)
-			return;
+    void KeyUp(int key, Keyboard::State& state)
+    {
+        if (key < 0 || key > 0xfe)
+            return;
 
-		auto ptr = reinterpret_cast<uint32_t*>(&state);
+        auto ptr = reinterpret_cast<uint32_t*>(&state);
 
-		unsigned int bf = 1u << (key & 0x1f);
-		ptr[(key >> 5)] &= ~bf;
-	}
+        unsigned int bf = 1u << (key & 0x1f);
+        ptr[(key >> 5)] &= ~bf;
+    }
 }
-
 
 #if !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP)
 
@@ -82,172 +80,160 @@ namespace
 class Keyboard::Impl
 {
 public:
-	Impl(Keyboard* owner) :
-		mState{},
-		mOwner(owner)
-	{
-		if (s_keyboard)
-		{
-			throw std::exception("Keyboard is a singleton");
-		}
+    Impl(Keyboard* owner) :
+        mState{},
+        mOwner(owner)
+    {
+        if (s_keyboard)
+        {
+            throw std::exception("Keyboard is a singleton");
+        }
 
-		s_keyboard = this;
-	}
+        s_keyboard = this;
+    }
 
-	~Impl()
-	{
-		s_keyboard = nullptr;
-	}
+    ~Impl()
+    {
+        s_keyboard = nullptr;
+    }
 
-	void GetState(State& state) const
-	{
-		memcpy(&state, &mState, sizeof(State));
-	}
+    void GetState(State& state) const
+    {
+        memcpy(&state, &mState, sizeof(State));
+    }
 
-	void Reset()
-	{
-		memset(&mState, 0, sizeof(State));
-	}
+    void Reset()
+    {
+        memset(&mState, 0, sizeof(State));
+    }
 
-	bool IsConnected() const
-	{
-		return true;
-	}
+    bool IsConnected() const
+    {
+        return true;
+    }
 
-	State           mState;
-	Keyboard*       mOwner;
+    State           mState;
+    Keyboard* mOwner;
 
-	static Keyboard::Impl* s_keyboard;
+    static Keyboard::Impl* s_keyboard;
 };
-
 
 Keyboard::Impl* Keyboard::Impl::s_keyboard = nullptr;
 
-
 void Keyboard::ProcessMessage(UINT message, WPARAM wParam, LPARAM lParam)
 {
-	auto pImpl = Impl::s_keyboard;
+    auto pImpl = Impl::s_keyboard;
 
-	if (!pImpl)
-		return;
+    if (!pImpl)
+        return;
 
-	bool down = false;
+    bool down = false;
 
-	switch (message)
-	{
-	case WM_ACTIVATEAPP:
-		pImpl->Reset();
-		return;
+    switch (message)
+    {
+    case WM_ACTIVATEAPP:
+        pImpl->Reset();
+        return;
 
-	case WM_KEYDOWN:
-	case WM_SYSKEYDOWN:
-		down = true;
-		break;
+    case WM_KEYDOWN:
+    case WM_SYSKEYDOWN:
+        down = true;
+        break;
 
-	case WM_KEYUP:
-	case WM_SYSKEYUP:
-		break;
+    case WM_KEYUP:
+    case WM_SYSKEYUP:
+        break;
 
-	default:
-		return;
-	}
+    default:
+        return;
+    }
 
-	int vk = static_cast<int>(wParam);
-	switch (vk)
-	{
-	case VK_SHIFT:
-		vk = MapVirtualKey((lParam & 0x00ff0000) >> 16, MAPVK_VSC_TO_VK_EX);
-		if (!down)
-		{
-			// Workaround to ensure left vs. right shift get cleared when both were pressed at same time
-			KeyUp(VK_LSHIFT, pImpl->mState);
-			KeyUp(VK_RSHIFT, pImpl->mState);
-		}
-		break;
+    int vk = static_cast<int>(wParam);
+    switch (vk)
+    {
+    case VK_SHIFT:
+        vk = MapVirtualKey((lParam & 0x00ff0000) >> 16, MAPVK_VSC_TO_VK_EX);
+        if (!down)
+        {
+            // Workaround to ensure left vs. right shift get cleared when both were pressed at same time
+            KeyUp(VK_LSHIFT, pImpl->mState);
+            KeyUp(VK_RSHIFT, pImpl->mState);
+        }
+        break;
 
-	case VK_CONTROL:
-		vk = (lParam & 0x01000000) ? VK_RCONTROL : VK_LCONTROL;
-		break;
+    case VK_CONTROL:
+        vk = (lParam & 0x01000000) ? VK_RCONTROL : VK_LCONTROL;
+        break;
 
-	case VK_MENU:
-		vk = (lParam & 0x01000000) ? VK_RMENU : VK_LMENU;
-		break;
-	}
+    case VK_MENU:
+        vk = (lParam & 0x01000000) ? VK_RMENU : VK_LMENU;
+        break;
+    }
 
-	if (down)
-	{
-		KeyDown(vk, pImpl->mState);
-	}
-	else
-	{
-		KeyUp(vk, pImpl->mState);
-	}
+    if (down)
+    {
+        KeyDown(vk, pImpl->mState);
+    }
+    else
+    {
+        KeyUp(vk, pImpl->mState);
+    }
 }
 
 #endif
-
-
 
 #pragma warning( disable : 4355 )
 
 // Public constructor.
 Keyboard::Keyboard() noexcept(false)
-	: pImpl(std::make_unique<Impl>(this))
+    : pImpl(std::make_unique<Impl>(this))
 {
 }
-
 
 // Move constructor.
 Keyboard::Keyboard(Keyboard&& moveFrom) noexcept
-	: pImpl(std::move(moveFrom.pImpl))
+    : pImpl(std::move(moveFrom.pImpl))
 {
-	pImpl->mOwner = this;
+    pImpl->mOwner = this;
 }
-
 
 // Move assignment.
 Keyboard& Keyboard::operator= (Keyboard&& moveFrom) noexcept
 {
-	pImpl = std::move(moveFrom.pImpl);
-	pImpl->mOwner = this;
-	return *this;
+    pImpl = std::move(moveFrom.pImpl);
+    pImpl->mOwner = this;
+    return *this;
 }
-
 
 // Public destructor.
 Keyboard::~Keyboard()
 {
 }
 
-
 Keyboard::State Keyboard::GetState() const
 {
-	State state;
-	pImpl->GetState(state);
-	return state;
+    State state;
+    pImpl->GetState(state);
+    return state;
 }
-
 
 void Keyboard::Reset()
 {
-	pImpl->Reset();
+    pImpl->Reset();
 }
-
 
 bool Keyboard::IsConnected() const
 {
-	return pImpl->IsConnected();
+    return pImpl->IsConnected();
 }
 
 Keyboard& Keyboard::Get()
 {
-	if (!Impl::s_keyboard || !Impl::s_keyboard->mOwner)
-		throw std::exception("Keyboard is a singleton");
+    if (!Impl::s_keyboard || !Impl::s_keyboard->mOwner)
+        throw std::exception("Keyboard is a singleton");
 
-	return *Impl::s_keyboard->mOwner;
+    return *Impl::s_keyboard->mOwner;
 }
-
-
 
 //======================================================================================
 // KeyboardStateTracker
@@ -255,26 +241,25 @@ Keyboard& Keyboard::Get()
 
 void Keyboard::KeyboardStateTracker::Update(const State& state)
 {
-	auto currPtr = reinterpret_cast<const uint32_t*>(&state);
-	auto prevPtr = reinterpret_cast<const uint32_t*>(&lastState);
-	auto releasedPtr = reinterpret_cast<uint32_t*>(&released);
-	auto pressedPtr = reinterpret_cast<uint32_t*>(&pressed);
-	for (size_t j = 0; j < (256 / 32); ++j)
-	{
-		*pressedPtr = *currPtr & ~(*prevPtr);
-		*releasedPtr = ~(*currPtr) & *prevPtr;
+    auto currPtr = reinterpret_cast<const uint32_t*>(&state);
+    auto prevPtr = reinterpret_cast<const uint32_t*>(&lastState);
+    auto releasedPtr = reinterpret_cast<uint32_t*>(&released);
+    auto pressedPtr = reinterpret_cast<uint32_t*>(&pressed);
+    for (size_t j = 0; j < (256 / 32); ++j)
+    {
+        *pressedPtr = *currPtr & ~(*prevPtr);
+        *releasedPtr = ~(*currPtr) & *prevPtr;
 
-		++currPtr;
-		++prevPtr;
-		++releasedPtr;
-		++pressedPtr;
-	}
+        ++currPtr;
+        ++prevPtr;
+        ++releasedPtr;
+        ++pressedPtr;
+    }
 
-	lastState = state;
+    lastState = state;
 }
-
 
 void Keyboard::KeyboardStateTracker::Reset() noexcept
 {
-	memset(this, 0, sizeof(KeyboardStateTracker));
+    memset(this, 0, sizeof(KeyboardStateTracker));
 }
