074449729ba774efe952b8a4772ccb43b5d907bb
Shuanglong <Shuanglong@JIANGSHUANGLONG>
2022-02-27 22:28:39 +0800

    获取鼠标位置窗口测试

--- QtGuiApplicationTest/QtGuiApplicationTest/Miscellaneous/MiscellaneousImageGrabber.cpp
+++ QtGuiApplicationTest/QtGuiApplicationTest/Miscellaneous/MiscellaneousImageGrabber.cpp
@@ -8,6 +8,7 @@
 #include <QThread>
 #include <QScreen>
 #include <QDesktopWidget>
+#include <QMouseEvent>
 
 #include "JCB_Logger/LogUtil.h"
 
@@ -16,6 +17,7 @@ MiscellaneousImageGrabber::MiscellaneousImageGrabber(QWidget* parent)
     , ui(new Ui::MiscellaneousImageGrabber())
 {
     ui->setupUi(this);
+    this->setMouseTracking(true);
 }
 
 MiscellaneousImageGrabber::~MiscellaneousImageGrabber()
@@ -43,9 +45,32 @@ MiscellaneousTestItem MiscellaneousImageGrabber::GetItemID()
     return MiscellaneousTestItem::QT_Image_Grabber;
 }
 
+void MiscellaneousImageGrabber::mouseMoveEvent(QMouseEvent* mouseEvent)
+{
+    QPoint tempPoint = mouseEvent->pos();
+    mMousePoint = this->mapToGlobal(tempPoint);
+    mCurMouseWin = ::WindowFromPoint(POINT(mMousePoint.x(), mMousePoint.y()));
+    
+    this->UpdateLogInfo();
+}
+
 void MiscellaneousImageGrabber::on_btnGrabWindow_clicked()
 {
-    int i = 0;
+    char objName[128] = { 0 };
+    DWORD bufLenNeed = 0;
+
+    std::string deskClassName;
+    HWND desktopWnd = ::GetDesktopWindow();
+    this->mWinRectList.clear();
+    this->GetWindowClassName(desktopWnd, deskClassName);
+    this->EnumChildWindowRecursively(desktopWnd);
+    this->UpdateLogInfo();
+
+    //HWINSTA hWinStation = GetProcessWindowStation();
+    //::GetUserObjectInformation(hWinStation, UOI_NAME, objName, 128, &bufLenNeed);
+    //::EnumDesktops(hWinStation, EnumDesktopProc, reinterpret_cast<LPARAM>(this));
+
+    //::EnumWindowStations(EnumWindowStationProc, reinterpret_cast<LPARAM>(this));
 }
 
 void MiscellaneousImageGrabber::on_btnGrabDesktop_clicked()
@@ -98,4 +123,79 @@ void MiscellaneousImageGrabber::on_btnGrabScreenAll_clicked()
 void MiscellaneousImageGrabber::on_btnGrabScrollArea_clicked()
 {
     int i = 0;
+}
+
+void MiscellaneousImageGrabber::UpdateLogInfo()
+{
+    static int i = 0;
+    RECT curWinRect = { 0 };
+    ::GetWindowRect(mCurMouseWin, &curWinRect);
+
+    QString logContent = QString("LogInfo Test %1\n").arg(++i);
+    logContent += QString("Cur Win : 0x%1\n").arg(QString("%1").arg(reinterpret_cast<long>(mCurMouseWin), 8, 16, QChar('0')).toUpper());;
+    logContent += QString("Mouse Point : (%1, %2)\n").arg(mMousePoint.x()).arg(mMousePoint.y());
+    logContent += QString("Mouse Win Rect : (%1, %2) (%3, %4)")
+        .arg(curWinRect.left).arg(curWinRect.top).arg(curWinRect.right - curWinRect.left).arg(curWinRect.bottom - curWinRect.top);
+    ui->teLogInfo->setText(logContent);
+}
+
+void MiscellaneousImageGrabber::GetWindowClassName(HWND hWnd, std::string& wndClassName)
+{
+    char className[128] = { 0 };
+    ::GetClassName(hWnd, className, 128);
+    wndClassName = className;
+}
+
+void MiscellaneousImageGrabber::EnumChildWindowRecursively(HWND parentWnd)
+{
+    ::EnumChildWindows(parentWnd, EnumChildWindowProc, reinterpret_cast<LPARAM>(this));
+}
+
+BOOL MiscellaneousImageGrabber::EnumChildWindowProcShadow(HWND childWnd)
+{
+    std::string wndClassName;
+    this->GetWindowClassName(childWnd, wndClassName);
+
+    if (::IsWindow(childWnd) && ::IsWindowVisible(childWnd))
+    {
+        RECT winRect = { 0 };
+        ::GetWindowRect(childWnd, &winRect);
+        mWinRectList.emplace_back(winRect);
+        //LogUtil::Debug(CODE_LOCATION, "childWnd=0x%08X wndClassName=%s", childWnd, wndClassName.c_str());
+    }
+    //LogUtil::Debug(CODE_LOCATION, "childWnd=0x%08X wndClassName=%s", childWnd, wndClassName.c_str());
+
+    ::EnumChildWindows(childWnd, EnumChildWindowProc, reinterpret_cast<LPARAM>(this));
+
+    return TRUE;
+}
+
+BOOL MiscellaneousImageGrabber::EnumDesktopProcShadow(LPTSTR lpszDesktop)
+{
+    LogUtil::Debug(CODE_LOCATION, "lpszDesktop=%s", lpszDesktop);
+    return TRUE;
+}
+
+BOOL MiscellaneousImageGrabber::EnumWindowStationProcShadow(LPTSTR lpszWindowStation)
+{
+    LogUtil::Debug(CODE_LOCATION, "lpszWindowStation=%s", lpszWindowStation);
+    return TRUE;
+}
+
+BOOL CALLBACK MiscellaneousImageGrabber::EnumChildWindowProc(_In_ HWND hwnd, _In_ LPARAM lParam)
+{
+    MiscellaneousImageGrabber* pThis = reinterpret_cast<MiscellaneousImageGrabber*>(lParam);
+    return pThis->EnumChildWindowProcShadow(hwnd);
+}
+
+BOOL CALLBACK MiscellaneousImageGrabber::EnumDesktopProc(_In_ LPTSTR lpszDesktop, _In_ LPARAM lParam)
+{
+    MiscellaneousImageGrabber *pThis = reinterpret_cast<MiscellaneousImageGrabber*>(lParam);
+    return pThis->EnumDesktopProcShadow(lpszDesktop);
+}
+
+BOOL CALLBACK MiscellaneousImageGrabber::EnumWindowStationProc(_In_ LPTSTR lpszWindowStation, _In_ LPARAM lParam)
+{
+    MiscellaneousImageGrabber* pThis = reinterpret_cast<MiscellaneousImageGrabber*>(lParam);
+    return pThis->EnumWindowStationProcShadow(lpszWindowStation);
 }
--- QtGuiApplicationTest/QtGuiApplicationTest/Miscellaneous/MiscellaneousImageGrabber.h
+++ QtGuiApplicationTest/QtGuiApplicationTest/Miscellaneous/MiscellaneousImageGrabber.h
@@ -3,6 +3,8 @@
 
 #include "MiscellaneousBase.h"
 
+#include <Windows.h>
+
 namespace Ui {class MiscellaneousImageGrabber;};
 
 class MiscellaneousImageGrabber : public MiscellaneousBase
@@ -18,6 +20,9 @@ public:
     virtual MiscellaneousTestGroup GetGroupID() override;
     virtual MiscellaneousTestItem GetItemID() override;
 
+protected:
+    virtual void mouseMoveEvent(QMouseEvent* mouseEvent) override;
+
 private slots:
     void on_btnGrabWindow_clicked();
     void on_btnGrabDesktop_clicked();
@@ -27,7 +32,21 @@ private slots:
     void on_btnGrabScrollArea_clicked();
 
 private:
+    void UpdateLogInfo();
+    void GetWindowClassName(HWND hWnd, std::string& wndClassName);
+    void EnumChildWindowRecursively(HWND parentWnd);
+    BOOL EnumChildWindowProcShadow(HWND childWnd);
+    BOOL EnumDesktopProcShadow(LPTSTR lpszDesktop);
+    BOOL EnumWindowStationProcShadow(LPTSTR lpszWindowStation);
+    static BOOL CALLBACK EnumChildWindowProc(_In_ HWND hwnd, _In_ LPARAM lParam);
+    static BOOL CALLBACK EnumDesktopProc(_In_ LPTSTR lpszDesktop, _In_ LPARAM lParam);
+    static BOOL CALLBACK EnumWindowStationProc(_In_ LPTSTR lpszWindowStation, _In_ LPARAM lParam);
+
+private:
     Ui::MiscellaneousImageGrabber *ui;
+    std::list<RECT>                mWinRectList;
+    HWND                           mCurMouseWin;
+    QPoint                         mMousePoint;
 };
 
 #endif // MISCELLANEOUS_IMAGE_GRABBER_H
